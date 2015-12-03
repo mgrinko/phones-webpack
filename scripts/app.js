479d00dc79357baf2b74b438d08b4e912e0059e5
Hrynko Mykhailo <glukym@gmail.com>
2015-12-03 22:09:48 +0200

    generators

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,40 @@
 'use strict';
 
-var Page = require('./page');
+require("babel-polyfill");
 
-var xhr = new XMLHttpRequest();
+function* app() {
+  var Page = require('./page');
 
-// 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-xhr.open('GET', 'phones/phones.json', true);
+  var response = yield fetch('phones/phones.json');
+  var phones = yield response.json();
 
-xhr.addEventListener('load', function() {
-  if (xhr.status != 200) {
-    // обработать ошибку
-    alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-  } else {
-    let menuItems = JSON.parse(xhr.responseText);
+  new Page({
+    el: document.querySelector('[data-component=page]'),
+    menuItems: phones
+  });
+}
 
-    let page = new Page({
-      el: document.querySelector('[data-component=page]'),
-      menuItems: menuItems
-    });
-  }
-});
+var generator = app();
+var result = generator.next();
 
-xhr.send();
+result.value
+  .then(function(data) {
+    return generator.next(data);
+  })
+  .then(function(data) {
+    return generator.next(data);
+  });
+
+
+//phonesRequest
+//  .then(function(response) {
+//    return response.json();
+//  })
+//  .then(function(menuItems) {
+//    new Page({
+//      el: document.querySelector('[data-component=page]'),
+//      menuItems: menuItems
+//    });
+//  });
+
 
